daebb9c4fc8dcbf1c27b368e5fbb46cc2cc69633
Bassam Ismail <skippednote@gmail.com>
2018-06-05 15:10:30 +0530

    IFE: Vanilla frontend setup.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Interview: Front-end
+
+### Background
+
+This exercise is to test your Front-end skills. In this repository you will find:
+
+1.  Project installation that you can setup with the instruction
+    shared below.
+2.  Link to Designs (PSDs and JPG files): [Here](https://drive.google.com/drive/folders/1fCEq4JLqH5taSY21VS5vm3AjkOhOlVeH?usp=sharing).
+
+Installation should configure everything you would need to get started theming based on the PSDs.
+
+### Prerequisites
+
+- [Node.js](https://nodejs.org/en/)
+- Browser: [Chrome](https://www.google.com/chrome/) or [Firefox](https://www.mozilla.org/en-US/firefox/new/)
+- Text editor: [VSCode](https://code.visualstudio.com/) or [Atom](https://atom.io/)
+
+### Installation
+
+- Install project dependencies using: `npm install` or `yarn install`.
+- Install `gulp-cli` globally using `npm install -g gulp-cli`.
+
+### Development
+
+- Start the server using `gulp server`. This will also compile assets for you.
+- Review the designs shared with you.
+- Break the components as per [Atomic Design](http://bradfrost.com/blog/post/atomic-web-design/)
+  into multiple templates that would be placed in the `templates` directory.
+- Following the same pattern Sass can be broken into partials that can be placed in `sass` directory. For naming classes follow [SMACSS](https://smacss.com/) and [BEM](http://getbem.com/).
+
+#### Brownie Points for:
+
+- Showcasing use of CSS Grids and Flex
+- Addressing a11y issues
+- Keeping things performant
+
+### Submission
+
+- Fork the repository.
+- From your fork create a branch named `IFE-NAME`, replacing `NAME` with your first name.
+- Submit a pull request.
---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+/* eslint-disable */
+const gulp = require('gulp');
+const twig = require('gulp-twig');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const glob = require('gulp-sass-glob');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+const stylelint = require('gulp-stylelint');
+const browserSync = require('browser-sync');
+
+gulp.task('twig', () => {
+  return gulp
+    .src('src/templates/*.html.twig')
+    .pipe(twig())
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass', () => {
+  return gulp
+    .src('src/sass/*.scss')
+    .pipe(
+      sass({
+        includePaths: ['./node_modules']
+      })
+    )
+    .pipe(glob())
+    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('stylelint', () => {
+  return gulp.src('src/sass/*.scss').pipe(
+    stylelint({
+      reporters: [{ formatter: 'string', console: true }]
+    })
+  );
+});
+
+gulp.task('babel', () => {
+  return gulp
+    .src('src/js/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('eslint', () => {
+  return gulp
+    .src(['src/js/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('browsersync', () => {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+  gulp.watch(
+    ['src/sass/**/*.scss', 'src/js/*.js'],
+    ['build', browserSync.reload]
+  );
+});
+
+gulp.task('lint', ['stylelint', 'eslint']);
+gulp.task('build', ['twig', 'sass', 'babel']);
+gulp.task('server', ['browsersync']);
+
+gulp.task('default', ['lint', 'build']);
